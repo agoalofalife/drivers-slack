53fe01edc1d2370ccffedd8d97fc370f532f6570
agoalofalife <agoalofalife@gmail.com>
2017-07-12 20:33:28 +0300

    add SlackAttachmentTest

---
+++ tests/Unit/SlackAttachmentTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+use FondBot\Drivers\Slack\SlackAttachment;
+use FondBot\Templates\Attachment;
+use Tests\TestCase;
+
+/**
+ * Class SlackAttachmentTest
+ * @package Tests\Unit
+ */
+class SlackAttachmentTest extends TestCase
+{
+    /**
+     * @var Attachment
+     */
+    protected $attachment;
+    protected $innerAttach;
+
+    public function setUp()
+    {
+        $this->attachment = new SlackAttachment();
+        $this->innerAttach = $this->faker()->words;
+        $test = ['attachments' => $this->innerAttach];
+
+        $this->attachment->setMetadata($test);
+    }
+
+    public function test_setMetadata()
+    {
+        $this->assertEquals(['attachments' => json_encode([$this->innerAttach])], $this->attachment->getMetadata());
+    }
+
+    public function test_getMetadata()
+    {
+        $this->assertInternalType('array', $this->attachment->getMetadata());
+    }
+}
