282d6c35aa35a7af31ec599521a5c97c76fe91f5
agoalofalife <agoalofalife@gmail.com>
2017-07-12 21:26:49 +0300

    ResponseMenuRequest add

---
+++ tests/Unit/TypeRequest/ResponseMenuRequest.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\TypeRequest;
+
+use FondBot\Drivers\Exceptions\InvalidRequest;
+use FondBot\Drivers\Slack\SlackDriver;
+use Tests\TestCase;
+use FondBot\Http\Request as HttpRequest;
+
+/**
+ * Class ResponseMenuRequest
+ * @package Tests\Unit\TypeRequest
+ */
+class ResponseMenuRequest extends TestCase
+{
+    /**
+     * @var ResponseMenuRequest
+     */
+    protected $responseMenuRequest;
+
+    /**
+     * @var HttpRequest
+     */
+    protected $request;
+
+    public function setUp()
+    {
+        $this->request        = $this->mock(HttpRequest::class);
+        $this->responseMenuRequest = new ResponseMenuRequest($this->request);
+    }
+
+    public function test_getUserId()
+    {
+        $json = '{"user" : {"id" : "1DA"}}';
+        $this->request->shouldReceive('getParameter')->once()->with('payload')->andReturn($json);
+        $this->responseMenuRequest->getUserId();
+    }
+
+    public function test_getChatId()
+    {
+        $json = '{"channel" : {"id" : "1DA"}}';
+        $this->request->shouldReceive('getParameter')->once()->with('payload')->andReturn($json);
+        $this->responseMenuRequest->getChatId();
+    }
+
+    public function test_verifyRequest()
+    {
+        $json = '{"token" : "sx"}';
+        $token   = $this->faker()->word;
+        $driver  = $this->mock(SlackDriver::class);
+        $this->request->shouldReceive('getParameter')->once()->with('payload')->andReturn($json);
+        $driver->shouldReceive('getParameter')->once()->with('verify_token')->andReturn($token);
+        $this->responseMenuRequest->verifyRequest($driver);
+    }
+
+    public function test_verifyRequest_exception()
+    {
+        $json    = '{"token" : "sx"}';
+        $driver  = $this->mock(SlackDriver::class);
+        $this->request->shouldReceive('getParameter')->once()->with('payload')->andReturn($json);
+        $driver->shouldReceive('getParameter')->once()->with('verify_token');
+        $this->expectException(InvalidRequest::class);
+        $this->responseMenuRequest->verifyRequest($driver);
+    }
+
+    public function test_getText()
+    {
+        $driver  = $this->mock(SlackDriver::class);
+        $json    = '{"actions" : [{ "value" : "as"}]}';
+        $this->request->shouldReceive('getParameter')->once()->with('payload')->andReturn($json);
+        $this->responseMenuRequest->getText($driver);
+    }
+}
