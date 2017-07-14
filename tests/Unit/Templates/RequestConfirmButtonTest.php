0ded93b24ecd413f3b1436d472f1590e4534c9dc
agoalofalife <agoalofalife@gmail.com>
2017-07-14 09:42:00 +0300

    RequestConfirmButtonTest test add

--- tests/Unit/Templates/RequestConfirmButtonTest.php
+++ tests/Unit/Templates/RequestConfirmButtonTest.php
@@ -24,4 +24,25 @@ class RequestConfirmButtonTest extends TestCase
         $this->assertArrayHasKey('confirm', $source);
         $this->assertArrayHasKeys(['title', 'text', 'ok_text', 'dismiss_text'], $source['confirm']);
     }
+
+    public function test_getName()
+    {
+        $this->assertEquals('RequestConfirmButton', $this->requestConfirmButton->getName());
+    }
+
+    public function test_setApproval()
+    {
+       $this->assertInstanceOf(RequestConfirmButton::class,  $this->requestConfirmButton->setApproval($this->faker()->word));
+    }
+
+    public function test_setDenial()
+    {
+        $this->assertInstanceOf(RequestConfirmButton::class,  $this->requestConfirmButton->setDenial($this->faker()->word));
+    }
+
+    public function test_setText()
+    {
+        $this->assertInstanceOf(RequestConfirmButton::class,  $this->requestConfirmButton->setText($this->faker()->word));
+    }
+
 }
